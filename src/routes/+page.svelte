26630d4272fe53fc4c4c8aab6de01f4528ba0a2f
farhan <177544242+farhan8883-max@users.noreply.github.com>
2025-10-06 10:22:24 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { supabase } from "../../lib/supabaseClient";
+  import { supabase } from "$lib/supabaseClient";
   import { goto } from "$app/navigation";
 
   // ==========================
